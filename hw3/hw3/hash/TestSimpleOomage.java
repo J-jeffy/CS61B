56d15206cc68b5df19008bdb571a1e1e0dea1959
J-jeffery <2458953250@qq.com>
2023-08-23 22:33:26 +0800

    hw3

--- hw3/hw3/hash/TestSimpleOomage.java
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -1,15 +1,14 @@
 package hw3.hash;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 
 
 import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 
 public class TestSimpleOomage {
@@ -25,10 +24,18 @@ public class TestSimpleOomage {
 
     @Test
     public void testHashCodePerfect() {
-        /* TODO: Write a test that ensures the hashCode is perfect,
-          meaning no two SimpleOomages should EVER have the same
-          hashCode UNLESS they have the same red, blue, and green values!
-         */
+        SimpleOomage ooA = new SimpleOomage(10, 5, 20);
+        SimpleOomage ooA1 = new SimpleOomage(10, 5, 20);
+        SimpleOomage ooA2 = new SimpleOomage(5, 10, 20);
+        SimpleOomage ooA3 = new SimpleOomage(20, 10, 5);
+        SimpleOomage ooB = new SimpleOomage(50, 50, 50);
+        HashSet<SimpleOomage> hashSet = new HashSet<>();
+        hashSet.add(ooA);
+        assertTrue(hashSet.contains(ooA1));
+        assertFalse(hashSet.contains(ooA2));
+        assertFalse(hashSet.contains(ooB));
+        assertFalse(hashSet.contains(ooA3));
+
     }
 
     @Test
@@ -42,15 +49,16 @@ public class TestSimpleOomage {
         assertNotEquals(ooA, "ketchup");
     }
 
-    /*
+
     @Test
     public void testHashCodeAndEqualsConsistency() {
         SimpleOomage ooA = new SimpleOomage(5, 10, 20);
         SimpleOomage ooA2 = new SimpleOomage(5, 10, 20);
+        System.out.println(ooA.equals(ooA2));//true
         HashSet<SimpleOomage> hashSet = new HashSet<>();
         hashSet.add(ooA);
         assertTrue(hashSet.contains(ooA2));
-    }*/
+    }
 
     /* TODO: Uncomment this test after you finish haveNiceHashCode Spread in OomageTestUtility */
     /*@Test
